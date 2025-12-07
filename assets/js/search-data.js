a4975a87e8fe39e58a655b55cc22d537fa91f264
Xiaoxi-Luo-CL <64958330+Xiaoxi-Luo-CL@users.noreply.github.com>
2025-12-07 19:56:42 +0000

    Deploying to gh-pages from @ Xiaoxi-Luo-CL/Xiaoxi-Luo-CL.github.io@87e5c8b1b4c7ed386bb00e2c8cc3f436d0047b64 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -48,9 +48,14 @@ ninja.data = [{
               window.location.href = "/books/the_godfather/";
             },},{id: "chinese_posts-引繩批根-考",
           title: '“引繩批根”考',
-          description: "2020年秋季秋季學期古代漢語讀書札記。對《史記·魏其武安侯列傳》中難以索解的“引繩批根”一詞进行了注解和探討。",
+          description: "",
           section: "Chinese_posts",handler: () => {
-              window.location.href = "/chinese_posts/2025-12-03-2020ancient-chinese/";
+              window.location.href = "/chinese_posts/2025-12-03-yinshengpigen/";
+            },},{id: "chinese_posts-對讀-老子-思考一則",
+          title: '對讀《老子》思考一則',
+          description: "2021年春季古代漢語讀書筆記。",
+          section: "Chinese_posts",handler: () => {
+              window.location.href = "/chinese_posts/2025-12-03-laozi/";
             },},{id: "news-i-started-my-internship-in-the-general-alignment-team-at-minimax",
           title: 'I started my internship in the General Alignment team at MiniMax!',
           description: "",
